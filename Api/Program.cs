aca9e6639617f7115f24e6fe1288ee1495d5036e
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-10-02 15:02:26 +0300

    feat(program.cs): configure google authentication

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,36 @@
+using Api.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookie";
+    options.DefaultChallengeScheme = "Google";
+})
+.AddCookie("Cookie")
+.AddGoogle(options =>
+{
+    builder.Configuration.Bind("Authentication:Google", options);
+});
+builder.Services.AddAuthorization();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Angular:Development", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var apiGroup = app.MapGroup("/api");
+AuthEndpoints.MapAuthEndpoints(apiGroup);
 
+app.UseCors("Angular:Development");
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
