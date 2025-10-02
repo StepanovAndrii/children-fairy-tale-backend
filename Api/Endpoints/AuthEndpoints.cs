7b75e2cc0aac90bae0da1e83036ffc568e5c6b74
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-10-02 15:01:04 +0300

    feat(api): build controllers for Google authentication

---
+++ Api/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Api.Endpoints
+{
+    public static class AuthEndpoints
+    {
+        public static void MapAuthEndpoints(this RouteGroupBuilder group)
+        {
+            var googleGroup = group.MapGroup("/auth/google");
+
+            googleGroup.MapGet("", ChallengeGoogle);
+        }
+
+        private static IResult ChallengeGoogle()
+        {
+            var props = new AuthenticationProperties
+            {
+                RedirectUri = "http://localhost:4200"
+            };
+
+            return Results.Challenge(props, ["Google"]);
+        }
+    }
+}
