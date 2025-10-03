de67119307844c60243a22db9c49a78cddb01b44
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-10-03 19:18:38 +0300

    feat(domain): add basic Email value object

---
+++ Domain/ValueObjects/Email.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.ValueObjects
+{
+    public class Email
+    {
+        public string Value { get; }
+
+        // TODO: Add validation logic
+        public Email(string value)
+        {
+            Value = value;
+        }
+    }
+}
