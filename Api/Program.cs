0f5eced02bb83b60d234d75408347e6cc5ceae54
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-10-13 19:51:35 +0300

    feat: add oauth workflow configuration

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,26 @@
 using Api.Endpoints;
+using Application.Interfaces.Services;
+using Application.Services;
+using Domain.Interfaces.Repositories;
+using Infrastructure.Mappings;
+using Infrastructure.Services;
+using Mapster;
+using MapsterMapper;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Contexts;
+using Persistence.Repositories;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
+
+MapsterConfiguration.RegisterMapsterConfig();
+
+builder.Services.AddSingleton(TypeAdapterConfig.GlobalSettings);
+builder.Services.AddScoped<IOAuthService, OAuthService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<MapsterMapper.IMapper, ServiceMapper>();
+builder.Services.AddScoped<Application.Interfaces.IMapper, MapsterMapperAdapter>();
 
 builder.Services.AddAuthentication(options =>
 {
@@ -13,6 +31,32 @@ builder.Services.AddAuthentication(options =>
 .AddGoogle(options =>
 {
     builder.Configuration.Bind("Authentication:Google", options);
+
+    options.Scope.Add("email");
+    options.Scope.Add("profile");
+
+    options.ClaimActions.MapJsonKey("pictureUrl", "picture");
+
+    options.Events.OnCreatingTicket = async context =>
+    {
+        using var scope = context.HttpContext.RequestServices.CreateScope();
+        var oauthService = scope.ServiceProvider.GetRequiredService<IOAuthService>();
+
+        var claims = context.Principal?.Claims.ToList();
+        var email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+        var googleId = claims?.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        var picture = claims?.FirstOrDefault(c => c.Type == "pictureUrl")?.Value;
+
+        if (string.IsNullOrWhiteSpace(email) 
+            || string.IsNullOrWhiteSpace(googleId))
+            return;
+
+        await oauthService.RegisterUserAsync(
+            googleId,
+            email,
+            picture
+        );
+    };
 });
 builder.Services.AddAuthorization();
 
@@ -34,7 +78,7 @@ builder.Services.AddDbContext<KazkaDbContext>(options =>
 var app = builder.Build();
 
 var apiGroup = app.MapGroup("/api");
-AuthEndpoints.MapAuthEndpoints(apiGroup);
+OAuthEndpoints.MapAuthEndpoints(apiGroup);
 
 app.UseCors("Angular:Development");
 app.UseAuthentication();
