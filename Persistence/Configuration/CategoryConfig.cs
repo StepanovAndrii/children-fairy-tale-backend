db4c8de4bfb7caf780b3fd56f3e3cd14dc4574af
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-10-17 15:01:28 +0300

    feat(domain): create new domain category entity

---
+++ Persistence/Configuration/CategoryConfig.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configuration
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder
+                .HasKey(category => category.Id);
+            builder
+                .HasAlternateKey(category => category.Name);
+            builder
+                .HasMany(category => category.Stories)
+                .WithOne(story => story.Category)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
