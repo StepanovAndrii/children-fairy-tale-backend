62a94d5270a26428ad4c1d4f8aa39de28e692338
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-10-17 15:08:53 +0300

    feat(persistence): create a new migration

--- Persistence/Migrations/20251015181816_Migration2.Designer.cs
+++ Persistence/Migrations/20251015181816_Migration2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence.Contexts;
@@ -11,9 +12,11 @@ using Persistence.Contexts;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(KazkaDbContext))]
-    partial class KazkaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251015181816_Migration2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
